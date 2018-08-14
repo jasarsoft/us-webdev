1703d552333943bec156d1b66d8af41206e19139
Edin Jašarević <edinjasar14@gmail.com>
2018-08-14 21:58:17 +0200

    pred03 | vezanje php koda

---
+++ predavanje03/Uvod.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title> Univerzitet Singidunum </title>
+</head>
+<body>
+    <h1> Univerzitet Singidunum </h1>
+    
+    <?php
+        echo '<b> Sada ucimo PHP!!!! </b>' 
+    ?>
+</body>
+</html>
