6ce1f078874aab17c1bb15d0cf0e2eed41ae8498
Edin Jašarević <edinjasar14@gmail.com>
2018-08-14 22:02:29 +0200

    pred03 | zanemarivanje bjelina

---
+++ predavanje03/bjeline.php
@@ -0,0 +1,17 @@
+<html>
+<head>
+    <title> Univerzitet Singidunum </title>
+</head>
+
+<body>
+    <?php echo '<b> Fakultet za informatiku i racunarstvo</b><br/>';
+
+        echo ' <b> Fakultet za  
+
+            informatiku i             racunarstvo </b> <br/>';
+
+        echo 'Fakultet za   informatiku', ' i racunarstvo';
+?>
+</body>
+
+</html>
