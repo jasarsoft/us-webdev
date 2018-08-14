ba9106698ea5864369f26eab0210159078c6a996
Edin Jašarević <edinjasar14@gmail.com>
2018-08-14 23:55:06 +0200

    operatori na nivu bita

---
+++ predavanje03/opr4.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title> Operatori 4 </title>
+</head>
+<body>
+<?php
+    // OPERATORI NA NIVOU BITA
+    $a=15;
+    $b=9;
+
+    $c=$a & $b;
+    $d=$a | $b;
+    $e=$a ^ $b;
+    $f=$a<<1;
+    $g=$b>>1;
+
+    echo "c=$c<br/>";
+    echo "d=$d<br/>";
+    echo "e=$e<br/>";
+    echo "f=$f<br/>";
+    echo "g=$g<br/>";
+?>
+</body>
