365eaf1ead226a91a76a21ab1a9bdf0d09a5a327
Edin Jašarević <edinjasar14@gmail.com>
2018-08-14 23:53:28 +0200

    operator sabiranjai modulo

---
+++ predavanje03/opr.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title> Operatori </title>
+</head>
+
+<body>
+    <?php
+        // aritmeticki operatori
+        $a=10;
+        $b=20;
+        $rezultat=$a+$b;
+
+        echo 'Rezultat sabiranja operanada $a i $b ', "sa vrednostima $a i $b je ", $rezultat, "<br/>";  
+
+        $rezultat=$a/$b;
+
+        echo 'Rezultat deljenja operanada $a i $b ', "sa vrednostima $a i $b je ", $rezultat, "<br/>";  
+
+        $rezultat=$a%$b;
+
+        echo 'Rezultat operatora modulo nad operandima $a i $b ', "sa vrednostima $a i $b je ", $rezultat, "<br/>";  
+    ?>
+</body>
+</html>
