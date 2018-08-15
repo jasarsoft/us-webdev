5fa3c23bee724b42e61ebdfa0e7d15b768cdc25e
Edin Jašarević <edinjasar14@gmail.com>
2018-08-15 15:37:08 +0200

    primjeri svih oblika komentara

---
+++ predavanje03/komantari.php
@@ -0,0 +1,16 @@
+<html>
+<head>
+    <title> Univerzitet Singidunum </title>
+</head>
+<body>
+    <?php
+        echo '<b> PHP podrzava komentare, koji su slicnu kao u C-u, C++-u i Javi. <b><br/>'; // komentar u jednom redu
+        echo '<b> Oznake za jednoredne komentare su // i #</b><br/>'; # komentar u jednom redu
+        echo '<b> Oznake za viseredne komentare su /* */</b> <br/>';
+        /* ovo je
+        viseredni
+        komentar 
+        */
+?>
+</body>
+</html>
