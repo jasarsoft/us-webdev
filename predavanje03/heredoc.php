63315441dcabc5c3794b8839aa86998de3f74c4f
Edin Jašarević <edinjasar14@gmail.com>
2018-08-15 15:38:36 +0200

    perl i heredoc nacin ispisa

---
+++ predavanje03/heredoc.php
@@ -0,0 +1,13 @@
+<html>
+<head>
+    <title> Singidunum Univerzitet </title>
+</head>
+<body>
+<?php
+    echo <<<START
+    ovo je stampanje
+    pomocu heredoc sintakse
+START;
+?>
+</body>
+</html>
