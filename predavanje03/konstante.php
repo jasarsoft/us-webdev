01c2490983d41bd77b99fdad07a66dcadd45c5df
Edin Jašarević <edinjasar14@gmail.com>
2018-08-15 15:36:57 +0200

    definicija i upotreba konstanti

---
+++ predavanje03/konstante.php
@@ -0,0 +1,12 @@
+<html>
+<head>
+    <title> Univerzitet Singidunum </title>
+</head>
+<body>
+    <?php
+        define('BEOGRAD',11000); //deklarisana je konstanta BEOGRAD koja ima vrednost 11000. Naravno konstanta je tipa int
+
+        echo 'Vrednost konstante BEOGRAD je ', BEOGRAD; //ispisujemo vrednost konstante BEOGRAD, izlaz je 11000.
+    ?>
+</body>
+</html>
