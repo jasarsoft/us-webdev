77eac0121501f2427665e07ee8022a9f31a4221c
Edin Jašarević <edinjasar14@gmail.com>
2018-09-13 15:15:57 +0200

    vj lek03 pr01 | echo

---
+++ lekcija03/primjer01/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<?php
+    $title = 'Naziv sajta';
+?>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>
+            <?php echo $title ?>
+        </title>
+    </head>
+    <body>
+        <?php
+            echo 'Sadrzaj';
+        ?>
+    </body>
+</html>
