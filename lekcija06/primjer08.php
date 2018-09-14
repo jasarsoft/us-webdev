5b80258860ef347f6910a098d5ed0de0ace2ac08
Edin Jašarević <edinjasar14@gmail.com>
2018-09-15 01:53:17 +0200

    vj lek06 pr08 | rjecnik

---
+++ lekcija06/primjer08.php
@@ -0,0 +1,13 @@
+<?php
+    $niz = [
+        'ime'           => 'Milan',
+        'prezime'       => 'Tair',
+        'instituacija'  => 'Univerzitet Singidunum',
+        0               => 123,
+        33              => 'Nesto'
+    ];
+    
+    echo $niz['prezime'];
+    echo '<br>';
+    echo $niz[0];
+    
