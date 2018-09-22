3aae22c744ccb07845d84472d47fdd4a2137d18f
Edin Jašarević <edinjasar14@gmail.com>
2018-09-22 17:00:04 +0200

    pred04 | do-while petlja

---
+++ predavanje04/do_while.php
@@ -0,0 +1,11 @@
+<?php
+
+$counter=0; //brojacka promenljiva
+
+// petlja ce da se izvrsi 6 puta
+do { 
+    echo "Brojac ima vrednost <font color=red> $counter </font> <br/>";
+    $counter=$counter+1;
+} while($counter<=5);
+
+?>
