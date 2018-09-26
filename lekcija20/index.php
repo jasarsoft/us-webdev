0119349c4845e8b4e6b5779777978a0dc8bab5a3
Edin Jašarević <edinjasar14@gmail.com>
2018-09-26 17:30:11 +0200

    lekcija20 | stranica proizvodi

--- lekcija20/index.php
+++ lekcija20/index.php
@@ -17,11 +17,27 @@
     
     require_once 'VebStranica.php';
     
-    $vs1 = new VebStranica("Stranica 'O nama' - Kompanija ta i ta", 
-            'Ovo je stracnia o nama s tekstom', 
-            'O nama', 
-            'tekst');
-    $vs1->setSadrzaj('<b>Poceli smo sa radom!<b><br>Kompanija postoji nekoliko godina...<br><br>Neki tekst o nama itd.');
-    $vs1->setSeoKljucneRijeci('O nama, stranica, upoznavanje, sadrzaj, posao');
+//    $vs1 = new VebStranica("Stranica 'O nama' - Kompanija ta i ta", 
+//            'Ovo je stracnia o nama s tekstom', 
+//            'O nama', 
+//            'tekst');
+//    $vs1->setSadrzaj('<b>Poceli smo sa radom!<b><br>Kompanija postoji nekoliko godina...<br><br>Neki tekst o nama itd.');
+//    $vs1->setSeoKljucneRijeci('O nama, stranica, upoznavanje, sadrzaj, posao');
+//    
+//    $vs1->generisiHTML();
+
+    $vs2 = new VebStranica('Stranica sa katalogom proizvoda', 'Ovdje mozete vidjeti nase prizvode...', 'Proizvodi', 'proizvodi');
+    $vs2->setPosebanSadrzaj([
+        [
+            'naslov' => 'Tastatura',
+            'opis'   => 'Najbolja gejmerska tastatura na trzistu.',
+            'cijena' => 23.5
+        ],
+        [
+            'naslov' => 'Mis',
+            'opis'   => 'Mis koji ima mogucnost podesavanja tegovima',
+            'cijena' => 17.8
+        ]
+    ]);
+    $vs2->generisiHTML();
     
-    $vs1->generisiHTML();
