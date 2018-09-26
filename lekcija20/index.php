7ff2bde238bd4d2387225c961a2f0f2168c519f1
Edin Jašarević <edinjasar14@gmail.com>
2018-09-26 17:22:42 +0200

    lekcija20 | oop

--- lekcija20/index.php
+++ lekcija20/index.php
@@ -1,18 +1,27 @@
 <?php
-    $seo_naslov = "Stranica 'O nama' - Kompanija ta i ta";
-    $naslov = 'O nama';
-    $godina = date('Y');
-    $proizvodi = [
-        [
-            'naslov' => 'Tastatura',
-            'opis'   => 'Najbolja gejmerska tastatura na trzistu.',
-            'cijena' => 23.5
-        ],
-        [
-            'naslov' => 'Mis',
-            'opis'   => 'Mis koji ima mogucnost podesavanja tegovima',
-            'cijena' => 17.8
-        ]
-    ];
+//    $seo_naslov = "Stranica 'O nama' - Kompanija ta i ta";
+//    $naslov = 'O nama';
+//    $godina = date('Y');
+//    $proizvodi = [
+//        [
+//            'naslov' => 'Tastatura',
+//            'opis'   => 'Najbolja gejmerska tastatura na trzistu.',
+//            'cijena' => 23.5
+//        ],
+//        [
+//            'naslov' => 'Mis',
+//            'opis'   => 'Mis koji ima mogucnost podesavanja tegovima',
+//            'cijena' => 17.8
+//        ]
+//    ];
     
-    require_once 'sablon_stranice.php';
+    require_once 'VebStranica.php';
+    
+    $vs1 = new VebStranica("Stranica 'O nama' - Kompanija ta i ta", 
+            'Ovo je stracnia o nama s tekstom', 
+            'O nama', 
+            'tekst');
+    $vs1->setSadrzaj('<b>Poceli smo sa radom!<b><br>Kompanija postoji nekoliko godina...<br><br>Neki tekst o nama itd.');
+    $vs1->setSeoKljucneRijeci('O nama, stranica, upoznavanje, sadrzaj, posao');
+    
+    $vs1->generisiHTML();
--- lekcija20/sablon_stranice.php
+++ lekcija20/sablon_stranice.php
@@ -1,8 +1,18 @@
 <!DOCTYPE html>
-<html>
+<html lang="<?php echo $this->getJezik(); ?>">
     <head>
-        <meta charset="UTF-8">
-        <title><?php echo htmlspecialchars($seo_naslov); ?></title>
+        <title><?php echo htmlspecialchars($this->getSeoNaslov()); ?></title>
+        
+        <?php if($this->getSeoKljucneRijeci()): ?>
+            <meta name="keywords" content="<?php echo htmlspecialchars($this->getSeoKljucneRijeci()); ?>">
+        <?php endif; ?>
+        
+        <?php if($this->getSeoOpisStranice()): ?>
+            <meta name="description" content="<?php echo htmlspecialchars($this->getSeoOpisStranice()); ?>">
+        <?php endif; ?>
+        <?php if($this->getIkonica()): ?>
+            <meta rel="shortcut icon" href="<?php echo htmlspecialchars($this->getIkonica()); ?>">
+        <?php endif; ?>
     </head>
     <body>
         <div id="wrapper">
@@ -19,18 +29,38 @@
                 </ul>
             </div>
             <div id="main">
-                <h1><?php echo htmlspecialchars($naslov); ?></h1>
+                <h1><?php echo htmlspecialchars($this->getNaslov()); ?></h1>
                 
-                <div class="products">
-                    <?php 
-                        foreach ($proizvodi as $proizvod){
-                            require 'sablon_proizvod.php';
-                        }
-                    ?>
-                </div>
+                <?php if($this->getTipStranice() == 'tekst'): ?>
+                    <div class="text">
+                        <?php echo $this->getSadrzaj(); ?>
+                    </div>
+                <?php elseif($this->getTipStranice() == 'proizvodi'): ?>
+                    <div class="products">
+                        <?php 
+                            if($this->getPosebanSadrzaj() and is_array($this->getPosebanSadrzaj())){
+                                foreach ($this->getPosebanSadrzaj() as $proizvod){
+                                    require 'sablon_proizvod.php';
+                                }
+                            }
+                        ?>
+                    </div>
+                <?php elseif($this->getTipStranice() == 'kontakt'): ?>
+                    <div class="kontakt">
+                        <?php if($this->getPosebanSadrzaj() and is_object($this->getPosebanSadrzaj())): ?>
+                            Ime i prezime: <?php echo htmlspecialchars($this->getPosebanSadrzaj()->imeIPrezime); ?><br>
+                            Telefon: <?php echo htmlspecialchars($this->getPosebanSadrzaj()->telefon); ?><br>
+                            e-Mail: <a href="mailto:<?php echo htmlspecialchars($this->getPosebanSadrzaj()->email); ?>">
+                                        <?php echo htmlspecialchars($this->getPosebanSadrzaj()->email); ?>
+                                    </a>
+                        <?php endif; ?>
+                    </div>
+                <?php else: ?>
+                    <!-- nepoznat tip stranice -->
+                <?php endif; ?>
             </div>
             <div id="footer">
-                &copy; <?php echo htmlspecialchars($godina); ?> - Ta i ta kompanija
+                &copy; <?php echo date('Y'); ?> - Ta i ta kompanija
             </div>
         </div>
     </body>
