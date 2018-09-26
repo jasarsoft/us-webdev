16d0afb743f8442c629ea49305ab0ab056987935
Edin Jašarević <edinjasar14@gmail.com>
2018-09-26 15:51:25 +0200

    lekcija20 | sablon proizvoda (div)

---
+++ lekcija20/sablon_proizvod.php
@@ -0,0 +1,9 @@
+<div class="poizvod">
+    <h2><?php echo htmlspecialchars($proizvod['naslov']); ?></h2>
+    <div class="opis">
+        <?php echo htmlspecialchars($proizvod['opis']); ?>
+    </div>
+    <div class="cijena">
+        <?php echo htmlspecialchars($proizvod['cijena']); ?> &euro;
+    </div>
+</div>
