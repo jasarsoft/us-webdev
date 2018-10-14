d943134917ce53a0decd451ca45427b36cc7c0d7
Edin Jašarević <edinjasar14@gmail.com>
2018-10-14 22:16:07 +0200

    lekcija10 | asocijativni niz

---
+++ lekcija10/niz.php
@@ -0,0 +1,23 @@
+<?php
+    $niz = [
+        [
+            "ime"       => 'Milan',
+            "prezime"   => 'Tair',
+            "email"     => 'mtair@singidunum.ac.rs'
+        ],
+        [
+            "ime"       => 'Marko',
+            "prezime"   => 'Sarac',
+            "email"     => 'msarac@singidunum.ac.rs'
+        ],
+        [
+            "ime"       => 'Dalibor',
+            "prezime"   => 'Radovanovic',
+            "email"     => 'dradovanovic@singidunum.ac.rs'
+        ],
+        [
+            "ime"       => 'Jasmina',
+            "prezime"   => 'Rakovic',
+            "email"     => 'jrakovic@singidunum.ac.rs'
+        ]
+    ];
