e877a6db05b350e2f147cb3b0f6d1df8afc27ba3
Edin Jašarević <edinjasar14@gmail.com>
2018-10-18 11:34:19 +0200

    pred04 | asocijativni niz

---
+++ predavanje04/nizovi1.php
@@ -0,0 +1,14 @@
+<?php
+
+//kreiranje niza kriptovalute 
+$kriptovalute=array("skuplji"=>"BTC",
+                    "srednji"=>"ETH",
+                    "jeftiniji"=>"DCR");
+
+// bice prikazana vrednost BTC
+echo $kriptovalute['skuplji'], "<br/>";
+
+// bice prikazana vrednost BTC
+echo $kriptovalute['srednji'], "<br/>";
+
+?>
