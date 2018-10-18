f3ecd2a213f8f89e88baa6e9875484069ccf50d6
Edin Jašarević <edinjasar14@gmail.com>
2018-10-18 11:34:38 +0200

    pred04 | niz funkcije range

---
+++ predavanje04/nizovi2.php
@@ -0,0 +1,15 @@
+<?php
+
+//kreiranje niza brojeva pomocu funkcije range
+$brojevi=range(1,100);
+
+//prikazuje broj 11
+echo $brojevi[10], "<br/>";
+
+
+$neparni=range(1,10,2);
+
+//prikazuje broj 5
+echo $neparni[2];
+
+?>
