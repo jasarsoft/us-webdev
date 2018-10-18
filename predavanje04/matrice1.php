fdd8447a9fb1f2061ceb2265d38b4eecc4973f1e
Edin Jašarević <edinjasar14@gmail.com>
2018-10-18 11:33:08 +0200

    pred04 | niz matrica

---
+++ predavanje04/matrice1.php
@@ -0,0 +1,12 @@
+<?php
+
+$automobili=array(array("BMW3","BMW5","BMW7"),
+                  array("Mercedes C klasa","Mercedes E klasa", "Mercedes S klasa"));
+
+//stampa 'BMW7'
+echo $automobili[0][2], "<br/>"; 
+
+//stampa 'Mercedes E klasa'
+echo $automobili[1][1];
+
+?>
