04568d7cd6c90878d4d81d614d031f5510bc71ec
Edin Jašarević <edinjasar14@gmail.com>
2018-10-18 11:34:01 +0200

    pred04 | niz

---
+++ predavanje04/nizovi.php
@@ -0,0 +1,13 @@
+<?php
+
+//kreiranje niza kriptovalute
+$kriptovalute=array("BTC","ETH","DCR","XMR","LTC");
+
+// bice prikazana vrednost BTC
+echo $kriptovalute[0], "<br/>";
+
+//dodavanje novog elementa niza
+$kriptovalute[5]="ETC";
+echo $kriptovalute[5];
+
+?>
