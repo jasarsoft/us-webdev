b14443b7cf59b86d1c1e1e2b2b940c8f2b8430cd
Edin Jašarević <edinjasar14@gmail.com>
2018-10-19 23:45:43 +0200

    pred04 | while

---
+++ predavanje04/while.php
@@ -0,0 +1,12 @@
+<?php
+
+$counter=0; //deklaracija i inicijalizacija brojacke promenljive
+
+while($counter<=5)
+{
+    echo "Brojac sada ima vrednost <font color=blue> $counter </font>";
+    echo "<br/>";
+    $counter++;
+}
+
+?>
