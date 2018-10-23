3351f03c7a3b0d9cc2250276e624adf43197acf0
Edin Jašarević <edinjasar14@gmail.com>
2018-10-23 23:16:52 +0200

    lekcija26 | layout after

---
+++ lekcija26/app/views/_global/afterContent.php
@@ -0,0 +1,8 @@
+            </main>
+            <footer id="footer">
+                &copy; <?php echo date('Y'); ?> - Kompanija ta i ta
+            </footer>
+        </section>
+    </body>
+</html>
+
