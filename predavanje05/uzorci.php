9e96333c58b3bccacf0c6e626175b50f5950a0ed
Edin Jašarević <edinjasar14@gmail.com>
2018-10-24 17:42:47 +0200

    pred05 | preg_match pattern

---
+++ predavanje05/uzorci.php
@@ -0,0 +1,10 @@
+<?php
+    $pattern = "/ca[kf]e/";
+    $text = "He was eating cake in the cafe.";
+
+    if(preg_match($pattern, $text)){
+        echo "Match found!";
+    } else{
+        echo "Match not found.";
+    }
+?>
