edf8466c731c513d3c849498d57052018fd2caac
Edin Jašarević <edinjasar14@gmail.com>
2018-10-27 22:58:30 +0200

    lekcija28 | view contact

---
+++ lekcija28/app/views/Contact/index.php
@@ -0,0 +1,29 @@
+<?php require_once 'app/views/_global/beforeContent.php'; ?>
+
+<article class="blok">
+    <header>
+        <h1>Kontakt forumalar:</h1>
+    </header>
+    
+    <form method="post" action="contact/send">
+        <div class="input-block">
+            <label form="f1_email">Adresa e-poste:</label>
+            <input type="email" name="email" id="f1_email" required class="input-field">
+        </div>
+        
+        <div class="input-block">
+            <label form="f1_subject">Naslov poruke:</label>
+            <input type="text" name="subject" id="f1_subject" required class="input-field">
+        </div>
+        
+        
+        <div class="input-block">
+            <label form="f1_message">Tekst poruke:</label>
+            <textarea name="message" rows="10" id="f1_message" required class="input-field"></textarea>
+        </div>
+        
+        <button type="submit" class="button">Posalji poruku</button>
+    </form>
+</article>
+
+<?php require_once 'app/views/_global/afterContent.php'; ?>
