fdaa9009e504b23001199e0e00faca5a9208194e
Edin Jašarević <edinjasar14@gmail.com>
2018-10-27 22:57:33 +0200

    lekcija28 | config

---
+++ lekcija28/Configuration.php
@@ -0,0 +1,10 @@
+<?php
+    final class Configuration {
+        const DB_HOST = 'localhost';
+        const DB_USER = 'root';
+        const DB_PASS = 'root';
+        const DB_BASE = 'sajt';
+        
+        const BASE = 'http://localhost:8080/lekcija28/';
+        const PATH = '/lekcija28/'; #na normalnom sajtu ce biti samo /
+    }
