65b27786486b6b186b6fa42af3e94415650eff05
Edin Jašarević <edinjasar14@gmail.com>
2018-10-29 17:02:36 +0100

    lekcija 26 | pogled kontakt

---
+++ lekcija26/app/views/Contact/handle.php
@@ -0,0 +1,13 @@
+<?php require_once 'app/views/_global/beforeContent.php'; ?>
+
+<article>
+    <header>
+        <h1>Kontakt forumlar</h1>
+    </header>
+    
+    <p><?php echo @$DATA['message']; ?></p>
+</article>
+
+
+
+<?php require_once 'app/views/_global/afterContent.php'; ?>
