d8b0f5f504a4f801e1a2b16c984eaf3a98501964
Edin Jašarević <edinjasar14@gmail.com>
2018-10-31 00:13:43 +0100

    lekcija31 | product view

---
+++ lekcija31/app/views/Products/index.php
@@ -0,0 +1,11 @@
+<?php require_once 'app/views/_global/beforeContent.php'; ?>
+
+<article class="blok">
+    <header>
+        <h1>List of products</h1>
+    </header>
+    
+    Ovdje je lista proizvoda koju vidi SAMO prijavljeni korisnik.
+</article>
+
+<?php require_once 'app/views/_global/afterContent.php'; ?>
