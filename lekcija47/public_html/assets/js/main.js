36f4e8559405b9a78915f7315d61846bb7d8b00d
Edin Jašarević <edinjasar14@gmail.com>
2018-10-31 15:20:50 +0100

    lekcija47 | func za provjeru tabele

--- lekcija47/public_html/assets/js/main.js
+++ lekcija47/public_html/assets/js/main.js
@@ -5,6 +5,22 @@ function prikaziTab(tabId) {
 
 function init(){
     prikaziTab(1);
+    provjeraTabele();
 }
 
 $(init);
+
+function provjeraTabele() {
+    $('#rezultati tbody tr').each(function() {
+       var tr = $(this);
+       var brojPoena = $(tr).attr('data-broj-poena');
+       
+       if (brojPoena < 11) {
+           $(tr).css('color', 'red');
+       } else if (brojPoena >= 11 && brojPoena < 17) {
+           $(tr).css('color', 'blue');
+       } else {
+           $(tr).css('color', 'green');
+       }
+    });
+}
