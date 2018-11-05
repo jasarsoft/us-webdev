cefc0c468c1568dae5a5e15eec801d1ac8bb57be
Edin Jašarević <edinjasar14@gmail.com>
2018-11-05 16:40:25 +0100

    lekcija33 | main view

---
+++ lekcija33/app/views/Main/index.php
@@ -0,0 +1,11 @@
+<?php require_once 'app/views/_global/beforeContent.php'; ?>
+
+<article class="blok">
+    <header>
+        <h1>Prijava na CMS</h1>
+    </header>
+    
+    <p><a href="<?php echo Configuration::BASE; ?>login">Login page</a></p>
+</article>
+
+<?php require_once 'app/views/_global/afterContent.php'; ?>
