bcde80616614473656e88752d5f9f636afbdc6c6
Edin Jašarević <edinjasar14@gmail.com>
2018-11-12 21:38:22 +0100

    lekcija36 | product add view

---
+++ lekcija36/app/views/Products/add.php
@@ -0,0 +1,33 @@
+<?php require_once 'app/views/_global/beforeContent.php'; ?>
+
+<article class="blok">
+    <header>
+        <h1>Add new product</h1>
+    </header>
+    
+    <form method="post" action="<?php echo Configuration::BASE; ?>product/add">
+        <div class="input-block">
+            <label form="f1_name">Name:</label>
+            <input type="text" name="name" id="f1_name" required class="input-field"">
+        </div>
+        
+        <div class="input-block">
+            <label form="f1_description">Description:</label>
+            <textarea name="description" rows="10" id="f1_description" required class="input-field"></textarea>
+        </div>
+        
+        <div class="input-block">
+            <label form="f1_price">Name:</label>
+            <input type="number" min="0.01" step="any" name="price" id="f1_price" required class="input-field">
+        </div>
+        
+        <button type="submit" class="button">Add new product</button>
+        <a class="button" href="<?php echo Configuration::BASE; ?>product/list">Back to the list</a>
+    </form>
+    
+    <?php if (isset($DATA['message'])): ?>
+    <p><?php echo htmlspecialchars($DATA['message']); ?></p>
+    <?php endif ?>
+</article>
+
+<?php require_once 'app/views/_global/afterContent.php'; ?>
