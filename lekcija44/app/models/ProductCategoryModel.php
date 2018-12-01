e7178c3f71459a036d1ff01eac736f801d6307fd
Edin Jašarević <edinjasar14@gmail.com>
2018-12-01 13:40:09 +0100

    lekcija44 | model kategorije proizvoda

---
+++ lekcija44/app/models/ProductCategoryModel.php
@@ -0,0 +1,29 @@
+<?php
+    class ProductCategoryModel implements ModelInterface {
+        public static function getAll(){
+            $SQL = 'SELECT * FROM product_category ORDER BY name;';
+            $prep = DataBase::getInstance()->prepare($SQL);
+            $prep->execute();
+            return $prep->fetchAll(PDO::FETCH_OBJ);
+        }
+        
+        public static function getById($product_category_id){
+            $product_category_id = intval($product_category_id);
+            $SQL = 'SELECT * FROM product_category WHERE product_category_id = ?;';
+            $prep = DataBase::getInstance()->prepare($SQL);
+            $prep->execute([$product_category_id]);
+            return $prep->fetch(PDO::FETCH_OBJ);
+        }
+        
+        public static function getProducts($product_category_id) {
+            $SQL = 'SELECT * FROM product_category_product WHERE product_category_id = ?;';
+            $prep = DataBase::getInstance()->prepare($SQL);
+            $prep->execute([$product_category_id]);
+            $items = $prep->fetchAll(PDO::FETCH_OBJ);
+            $list = [];
+            foreach ($items as $item) {
+                $list[] = ProductModel::getById($item->product_id);
+            }
+            return $list;
+        }
+    }
