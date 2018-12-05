27f8a8757d54c89d45541a30bc6048b8bae8ec48
Edin Jašarević <edinjasar14@gmail.com>
2018-12-05 23:53:02 +0100

    lekcija44 | config class

---
+++ lekcija44/Configuration.php
@@ -0,0 +1,12 @@
+<?php
+    final class Configuration {
+        const DB_HOST = 'localhost';
+        const DB_USER = 'root';
+        const DB_PASS = 'root';
+        const DB_BASE = 'sajt';
+        
+        const BASE = 'http://localhost:8080/lekcija44/';
+        const PATH = '/lekcija44/'; #na normalnom sajtu ce biti samo /
+        
+        const SALT = '8398djsad81hd872z3eds71eh';
+    }
