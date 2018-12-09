68feacdc6b4758668395141c1a73d0bfd707a3a2
Edin Jašarević <edinjasar14@gmail.com>
2018-12-09 15:55:30 +0100

    lekcija64 | index file

---
+++ lekcija64/index.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * This is a staring point of the web application.
+ */
+
+namespace Project;
+
+# Inteface
+require_once './interfaces/Identifiable.php';
+
+# Classes
+require_once './classes/Person.php';
+require_once './classes/Student.php';
+require_once './classes/MasterStudent.php';
+require_once './classes/Employee.php';
+
+# Demo
