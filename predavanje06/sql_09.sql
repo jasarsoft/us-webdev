0f5d454b256629641cc3eb099fca4d289ff4d297
Edin Jašarević <edinjasar14@gmail.com>
2019-02-24 19:26:53 +0100

    sql | insert

---
+++ predavanje06/sql_09.sql
@@ -0,0 +1,4 @@
+-- INSERT INTO
+-- Ubacivanje redova navodjenjem kolona
+INSERT INTO employees (birth_date, first_name, last_name, gender,hire_date) 
+VALUES ('1985/10/12','Marko','Markovic','M','2000/01/01');
