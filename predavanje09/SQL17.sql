8628184c294efe3d9179dcf4659ec493ec9ff6f6
Edin Jašarević <edinjasar14@gmail.com>
2019-03-02 13:09:20 +0100

    pred09 sql | rad sa karakterima

---
+++ predavanje09/SQL17.sql
@@ -0,0 +1,8 @@
+-- funkcije za rad sa karakterima
+USE employees;
+-- funkcija format koja zaokruzuje decimalni broj
+SELECT FORMAT(250,23232,2);
+-- funkcija insert
+SELECT INSERT("Veb Programiranje",1,3,"Web");
+-- INSTR funkcija
+SELECT INSTR("Veb Programiranje","Programiranje");
