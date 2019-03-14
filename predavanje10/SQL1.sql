8d6ca277897579b72d64cf66f3e46caaeac69ffe
Edin Jašarević <edinjasar14@gmail.com>
2019-03-14 15:36:58 +0100

    pred10 php | create user

---
+++ predavanje10/SQL1.sql
@@ -0,0 +1,6 @@
+-- CREATE I DROP USER
+-- kreira se korisnik nebojsa na lokalnom racunaru
+CREATE USER 'nebojsa'@'localhost' IDENTIFIED BY 'sifra';
+-- brise se korisnik nebojsa sa lokalnog racunara
+DROP USER 'nebojsa'@'localhost';
+CREATE USER 'nebojsa'@'localhost' IDENTIFIED BY 'sifra';
