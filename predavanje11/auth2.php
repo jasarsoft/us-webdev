fabca04ed38821fa83097fbcdf4ca545e7660470
Edin Jašarević <edinjasar14@gmail.com>
2019-03-16 18:50:56 +0100

    pred11 php | cookies

---
+++ predavanje11/auth2.php
@@ -0,0 +1,21 @@
+﻿<?php 
+
+$_SERVER['PHP_AUTH_USER']='nebojsa';
+$_SERVER['PHP_AUTH_PW']='what2ucare';
+
+$autorizovan = FALSE;
+if (isset($_SERVER['PHP_AUTH_USER']) && isset($_SERVER['PHP_AUTH_PW'])) 
+{$sifLoz = file("sifrovaneLozinke.txt");
+
+if (in_array($_SERVER['PHP_AUTH_USER'].":"
+.md5($_SERVER['PHP_AUTH_PW'])."\n", $sifLoz))
+$autorizovan = TRUE;}
+
+if (! $autorizovan) {
+header('WWW-Authenticate: Basic Realm');
+header('HTTP/1.0 401 Neautorizovan');
+/* Ako korisnik ukuca Cancel dobija se sledeće */
+print('Morate dati ispravne login podatke!');
+exit;}
+echo "Materijal sa restrikcijama ..."
+?>
